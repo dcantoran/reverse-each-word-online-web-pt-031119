8b557dcb98ab262479034ddd59f28820593299a6
David Cantoran <davidcantoran36@gmail.com>
2019-03-20 02:22:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(str)
-  revers = str.split
-  arr = []
-  revers.each do |word|
-    arr << word.reverse
-  end
-  arr.join(" ")
-end 
+# def reverse_each_word(str)
+#   revers = str.split
+#   arr = []
+#   revers.each do |word|
+#     arr << word.reverse
+#   end
+#   arr.join(" ")
+# end 
 
 def reverse_each_word(str)
   rev = str.split(" ")
